aeec62f063d933ca78ffa14a060d4f33a9517635
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2023-05-30 00:11:16 +0900

    Fix: modify cls function
    - add function input type
    - filter undefined, false input type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export const cls = (...className: string[]) => {
-  return className.join(' ');
+export const cls = (...classNames: Array<string | undefined | false>) => {
+  return classNames.filter((className) => !!className).join(' ');
 };
